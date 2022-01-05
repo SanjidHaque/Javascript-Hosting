d28626a60d89a3abfc5dd2d5f46a1fc368c69326
Sanjid Haque <sanjidulhaque@gmail.com>
2022-01-05 18:14:10 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+if (confirm("Press a button")) {
+
+  txt = "You pressed OK!";
+  var btn = document.createElement("BUTTON");   // Create a <button> element
+  btn.innerHTML = "Click Me";                   // Insert text
+  document.body.appendChild(btn);   
+
+} else {
+  txt = "You pressed Cancel!";
+}
