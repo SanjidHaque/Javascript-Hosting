8fa1b8f2cda2a7c1a88ddb4757553d5c6c83d1e8
Sanjid Haque <sanjidulhaque@gmail.com>
2022-01-09 13:49:54 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-if (confirm("Press a button")) {
+function callConfirmBox () {
+  if (confirm("Press a button")) {
 
-  txt = "You pressed OK!";
-  var btn = document.createElement("BUTTON");   // Create a <button> element
-  btn.innerHTML = "Click Me";                   // Insert text
-  document.body.appendChild(btn);   
-
-} else {
-  txt = "You pressed Cancel!";
+    txt = "You pressed OK!";
+    var btn = document.createElement("BUTTON");   // Create a <button> element
+    btn.innerHTML = "Click Me";                   // Insert text
+    document.body.appendChild(btn);   
+  
+  } else {
+    txt = "You pressed Cancel!";
+  }
 }
